ea84ffc72fcfd91dbdfc315f224e068f0d170b95
Kain Cortrecht <kaincortrecht@Kains-MacBook-Air.local>
2022-01-17 22:45:07 -0500

    tasks done, almost finished

--- index.js
+++ index.js
@@ -81,8 +81,11 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning, number){
+  return{
+    Home: inning(number),
+    Away: inning(number)
+  }
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +93,11 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningcb) {
+  return {
+    Home: inningcb(),
+    Away: inningcb()
+  }
 }
 
 
